297bd683f0a1c89719be44c303e4483e15a594d8
maurice <maurice@maurices-macbook-pro.home>
2016-02-25 18:54:30 +0100

    Merge origin/master
    Conflicts:
    	src/com/lagalerie/domain/Shop.java

--- src/test01/Main.java
+++ src/test01/Main.java
@@ -15,8 +15,8 @@ public class Main {
     public static void main(String[] args) {
         try {
             GoldenBookService gold = new GoldenBookService(loadProps("/setup.properties"));
-            PhotoInfo photo = gold.uploadPhoto(new File("/Users/maurice/perry.ch/hikes/_site/2015-12-20/preview/_DSF2480.jpg"), "Tannay", "La taverne");
-            Album album = gold.createAlbum("Album Tannay", "Album Tannay description", photo.getId());
+            PhotoInfo photo = gold.uploadPhoto(new File("/Users/maurice/Downloads/_DSF2241.jpg"), "Châtelet", "Roc du Châtelet");
+            Album album = gold.createAlbum("Album Bovonne", "Album Bovonne description", photo.getId());
             String lastId = null;
             for (Album a: gold.getAlbums()) {
                 printAlbum(a);
